38b9e4f92e12521d88631f622e39afa46016c37a
alexandrarosario4 <79703725+alexandrarosario4@users.noreply.github.com>
2022-12-21 09:51:15 -0400

    Update main_final.py

--- main_final.py
+++ main_final.py
@@ -10,7 +10,7 @@ import plotly.express as px
 import streamlit as st
 import numpy as np
 
-df_og = pd.read_csv('final.csv', sep=";")
+df_og = pd.read_csv('https://raw.githubusercontent.com/alexandrarosario4/proyectoFinal/main/final.csv', sep=";")
 hombres = df_og.drop(df_og.loc[df_og['Event']=="100 m hurdles"].index)
 mujeres = df_og.drop(df_og.loc[df_og['Event']=="110 m hurdles"].index)
 
@@ -31,7 +31,7 @@ st.set_page_config(page_title="National Records in Athletics by Country",
                     layout="wide",
                     initial_sidebar_state="expanded")
 
-px.set_mapbox_access_token(open('.mapbox_token').read())
+px.set_mapbox_access_token(open('https://raw.githubusercontent.com/alexandrarosario4/proyectoFinal/main/.mapbox_token').read())
 st.title("National Records in Athletics by Country")
 
 with st.sidebar:
